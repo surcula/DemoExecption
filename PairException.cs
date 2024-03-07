80d112b83c1fc666ef7ccc0fb90e7cf565dd7777
david gaillet <david.gaillet@live.fr>
2024-03-07 10:31:16 +0100

    Ajoutez des fichiers projet.

---
+++ PairException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoExecption
+{
+    internal class PairException : Exception
+    {
+		private string _message;
+
+		public string Message
+		{
+			get { return _message; }
+			set { _message = "Nombre impair interdit"; }
+			
+
+		}
+
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using DemoExecption;
+
+try
+{
+	int x = 42;
+	x = int.Parse(Console.ReadLine());
+	if (x % 2 == 0) throw new PairException();
+
+    Console.WriteLine(x/0);
+}
+catch (FormatException ex)
+{
+    Console.WriteLine("Entrez un nombre s'il vous plait");
+   
+}
+catch(DivideByZeroException) 
+{ 
+	Console.WriteLine("Erreur de division"); 
+}
+
+catch(PairException pe)
+{
+    Console.WriteLine(pe.Message);
+}
+catch(Exception e)
+{
+    Console.WriteLine(e.Message);
+}
